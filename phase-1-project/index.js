d8c0e57a39a222d61a02978b15af8b5d1b33cd0e
Bridget Akellian <akellianbv@gmail.com>
2022-04-06 19:25:15 -0400

    adding render function

--- phase-1-project/index.js
+++ phase-1-project/index.js
@@ -1,13 +1,35 @@
+
+
 document.addEventListener("DOMContentLoaded", () =>{
-    function fetchMovies() {
+      
+    const movieCollection = document.getElementById('movie-collection') 
+    function renderMovie(movie) {
+        const movieInfo = 
+          `<div class="card">
+            <h2>${movie.name}</h2>
+            <p>Rotten Tomatoes Score: ${movie.rottenTomatoesScore}</p>
+            <p>Box Office Revenue: ${movie.boxOfficeRevenueInMillions} million</p>
+            <p></p>
+            <button class="like-btn" id="${movie.id}">Like ❤️</button>
+          </div>`
+        
+          movieCollection.innerHTML += movieInfo
+      }
+
+    function fetchMovie() {
         fetch("https://the-one-api.dev/v2/movie", {headers: {Authorization: 'Bearer 07MCBm8QqP0vQVu9yGIR'}})
         .then(response => response.json())
-        .then(result => console.log(result))
+        .then(result => {
+            console.log(result)
+         return   result.docs.forEach(renderMovie)
+        })
         .catch(error => console.log('error', error));
     }
+    fetchMovie()
+     
 })
 
-fetchMovies()
+
 
 
 
