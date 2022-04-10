0624550672d01dc4d6f02e50f8a3c6cfe39ed5ab
Bridget Akellian <akellianbv@gmail.com>
2022-04-10 17:46:32 -0400

    cleaning up js

--- phase-1-project/index.js
+++ phase-1-project/index.js
@@ -1,49 +1,9 @@
-
-// at this point in the code this collection will be empty
-// we have to wait for the html to be rendered once the network call returns.
-// const postCommentBtn = document.getElementsByClassName('comment-btn');
-console.log("loaded")
-
-function setupEventHandlers (postCommentBtns) {
-    console.log("setupEventHandlers 1");
-
-    // using spread operator to change html collection to array
-    // can also use Array.from()
-    [...postCommentBtns].forEach(btn => {
-        btn.addEventListener('click', function() {
-          event.preventDefault();
-          console.log('it was clicked')
-          let commentValue = document.getElementById("comment-box").value; 
-
-          let li = document.createElement("li");
-          let text = document.createTextNode(commentValue);
-          li.appendChild(text);
-            //comments are only appending to the first box - how do I get them to append to the box the user is on
-          document.getElementById("unordered").appendChild(li);
-          
-          //need to work on clearing comment box after comment is sent 
-      })
-    })
-}
-// setupEventHandlers(document.getElementsByClassName('comment-btn'))
-
-// function setUpVoteButtons (voteButtons) {
-//     console.log(voteButtons)
-//     [...voteButtons].forEach(btn => {
-//         btn.addEventListener('click', function() {
-//             event.preventDefault();
-//             console.log('vote button was clicked!')
-//             let voteCount = document.
-//         })
-//     })
-// }
-
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOMContentLoaded 1")
-    const movieCollection = document.getElementById('movie-collection') 
+
     function renderMovie(movie) {
         console.log("renderMovie 3")
-        const movieInfo = 
+        let movieInfo = 
           `<div class="card">
             <h2>${movie.name}</h2>
             <p><b>Rotten Tomatoes Score:</b> ${movie.rottenTomatoesScore}</p>
@@ -61,48 +21,59 @@ document.addEventListener("DOMContentLoaded", () => {
           </div>`
         
           movieCollection.innerHTML += movieInfo
-      }
+    }
 
     function fetchMovie() {
         fetch("https://the-one-api.dev/v2/movie", {headers: {Authorization: 'Bearer 07MCBm8QqP0vQVu9yGIR'}})
         .then(response => response.json())
+        .then(data => movies = data) // assigning the fetch to a variable
         .then(result => {
+            console.log(movies) //this is to see the movie collection in the console
             window.movies = result.docs
             result.docs.forEach(renderMovie)
             setupEventHandlers(document.getElementsByClassName('comment-btn'))
         })
         .catch(error => console.log('error', error));
     }
+
+    function setupEventHandlers (postCommentBtns) {
+        console.log("setupEventHandlers 1");
+    
+        // using spread operator to change html collection to array
+        // can also use Array.from()
+        [...postCommentBtns].forEach(btn => {
+            btn.addEventListener('click', function() {
+              event.preventDefault();
+              console.log('it was clicked')
+              let commentValue = document.getElementById("comment-box").value; 
+    
+              let li = document.createElement("li");
+              let text = document.createTextNode(commentValue);
+              li.appendChild(text);
+                //comments are only appending to the first box - how do I get them to append to the box the user is on
+              document.getElementById("unordered").appendChild(li);
+              
+              //need to work on clearing comment box after comment is sent 
+          })
+        })
+    }
+    
     
     console.log('fetching movies')
     fetchMovie()
 
-    //filter buttons:
-    // 1. click a button to show movies over 200 
-    // 2. click a button to show mover under 200 
-    // 3. click for all movies 
 
-    function fliterMovies(movieArr) {
-        // filter the data / show the data
-        //heres the array - make it only 200 min movies 
-        //try doing this in the console first and then move forward 
-    }
  
     let voteButtons = document.getElementsByClassName('vote-btn')
-    console.log(voteButtons)
-
     let longMoviesBtn = document.getElementById("long-movies-btn")
-    console.log(longMoviesBtn)
-
     let shortMoviesBtn = document.getElementById("short-movies-btn")
-    console.log(shortMoviesBtn)
+    const movieCollection = document.getElementById('movie-collection') 
 
-
-
-
+    //working filter functions:
+    // let shortMovieArr = movies.filter((movie) => movie.runtimeInMinutes < 200)
+    // let longMovieArr= movies.filter((movie) => movie.runtimeInMinutes > 200)
+    
 })
-
-console.log("step 100000")
 
 
 //concept:
