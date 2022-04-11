fd147d9436c92e86c6e9e11b0e554e82d7c2037c
Bridget Akellian <akellianbv@gmail.com>
2022-04-11 10:47:27 -0400

    Fixed JS comment bugs

--- phase-1-project/index.js
+++ phase-1-project/index.js
@@ -12,11 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
             <p><b>Box Office Revenue:</b> ${movie.boxOfficeRevenueInMillions} million</p>
             
             <h3>comments:</h3>
-                <ul id="unordered">
+                <ul id="${movie._id}">
 
                 </ul>
-            <input type="text" id="comment-box" placeholder="Enter comment">
-            <button class="comment-btn" id="${movie.id}">COMMENT</button>
+            <form id="comment-form">
+                <input type="text" id="comment-box" placeholder="Enter comment">
+                <button class="comment-btn" id="${movie._id}">COMMENT</button>
+            </form>
             <button class="vote-btn" id="${movie.id}">VOTE</button>
           </div>`
         
@@ -28,33 +30,59 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(response => response.json())
         .then(data => movies = data) // assigning the fetch to a variable
         .then(result => {
-            console.log(movies) //this is to see the movie collection in the console
+            console.log("all movies:", movies) //this is to see the movie collection in the console
             window.movies = result.docs
             result.docs.forEach(renderMovie)
-            setupEventHandlers(document.getElementsByClassName('comment-btn'))
+            setupEventHandlers()
+            let shortMovieArr = movies.filter((movie) => movie.runtimeInMinutes < 200) //can set up variable in this function
+            let longMovieArr = movies.filter((movie) => movie.runtimeInMinutes > 200)
+            console.log('short arr:', shortMovieArr)
+            console.log('long movie arr:', longMovieArr)
+
+            function showShortMovies() {
+                let shortMoviesBtn = document.getElementById("short-movies-btn")
+                shortMoviesBtn.addEventListener('click', function() {
+                    console.log('clicked!')
+                    console.log(shortMovieArr);
+                    // return only the movies shorter than '200 minutes' 
+                })
+            }
+            showShortMovies();
         })
         .catch(error => console.log('error', error));
     }
 
-    function setupEventHandlers (postCommentBtns) {
+    // function showShortMovies() {
+    //     shortMoviesBtn.addEventListener('click', function() {
+    //         console.log('clicked!')  
+    //     })
+    // }
+    // showShortMovies()
+
+    function setupEventHandlers () {
         console.log("setupEventHandlers 1");
     
+        document.getElementById('movie-collection').addEventListener('submit', (e) => {
+            e.preventDefault()
+            console.log("comment button:", e.target.parentElement.querySelector('ul'))
+            const ul = e.target.parentElement.querySelector('ul')
+        // })
         // using spread operator to change html collection to array
         // can also use Array.from()
-        [...postCommentBtns].forEach(btn => {
-            btn.addEventListener('click', function() {
-              event.preventDefault();
+        // Array.from(document.getElementsByClassName('comment-btn')).forEach(btn => {
+            // btn.addEventListener('click', () => {
+                
               console.log('it was clicked')
-              let commentValue = document.getElementById("comment-box").value; 
+              let commentValue = e.target.parentElement.querySelector("#comment-box").value; 
     
               let li = document.createElement("li");
               let text = document.createTextNode(commentValue);
               li.appendChild(text);
                 //comments are only appending to the first box - how do I get them to append to the box the user is on
-              document.getElementById("unordered").appendChild(li);
-              
-              //need to work on clearing comment box after comment is sent 
-          })
+              ul.appendChild(li);
+              e.target.parentElement.querySelector("#comment-box").value = ""
+            
+        //   })
         })
     }
     
@@ -66,7 +94,7 @@ document.addEventListener("DOMContentLoaded", () => {
  
     let voteButtons = document.getElementsByClassName('vote-btn')
     let longMoviesBtn = document.getElementById("long-movies-btn")
-    let shortMoviesBtn = document.getElementById("short-movies-btn")
+    
     const movieCollection = document.getElementById('movie-collection') 
 
     //working filter functions:
@@ -80,4 +108,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // allMovies = getMovies() 
 //these are functions: 
 // longMovies = moviesLongerThanMins(minutes, movies) // return only the movies longer than 'minutes'
-// shortMovies = moviesShorterThanMins(minutes, movies) // return only the movies shorter than 'minutes' 
+// shortMovies = moviesShorterThanMins(minutes, movies) // return only the movies shorter than '200 minutes' 
